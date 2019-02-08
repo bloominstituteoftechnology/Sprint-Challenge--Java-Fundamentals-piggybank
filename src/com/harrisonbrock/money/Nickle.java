50777db041bb7c1254c55f04955c85c0f851aaed
Harrison Brock <harrison@harrisonbrock.com>
2019-02-08 12:18:09 -0500

    Added Nickle

---
+++ src/com/harrisonbrock/money/Nickle.java
@@ -0,0 +1,32 @@
+package com.harrisonbrock.money;
+
+public class Nickle implements Coin{
+    private double totalAmount;
+    private int amount;
+    private final double VALUE = 0.05;
+
+    public Nickle() {
+        amount = 1;
+        this.totalAmount = amount * VALUE;
+    }
+
+    public Nickle(int amount) {
+        this.amount = amount;
+        this.totalAmount= amount * VALUE;
+    }
+
+    @Override
+    public int amountInserted() {
+        return amount;
+    }
+
+    @Override
+    public String getName() {
+        return "Nickle";
+    }
+
+    @Override
+    public double getAmount() {
+        return totalAmount;
+    }
+}
