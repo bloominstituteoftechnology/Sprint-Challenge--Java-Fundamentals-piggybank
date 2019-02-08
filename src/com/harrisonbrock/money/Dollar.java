3a265c12ca0c55fc5ddd4bbdb4e21025db60641d
Harrison Brock <harrison@harrisonbrock.com>
2019-02-08 12:19:58 -0500

    Added Dollar

---
+++ src/com/harrisonbrock/money/Dollar.java
@@ -0,0 +1,32 @@
+package com.harrisonbrock.money;
+
+public class Dollar implements Coin {
+    private double totalAmount;
+    private int amount;
+    private final double VALUE = 1;
+
+    public Dollar() {
+        amount = 1;
+        this.totalAmount = amount * VALUE;
+    }
+
+    public Dollar(int amount) {
+        this.amount = amount;
+        this.totalAmount= amount * VALUE;
+    }
+
+    @Override
+    public int amountInserted() {
+        return amount;
+    }
+
+    @Override
+    public String getName() {
+        return "Dollar";
+    }
+
+    @Override
+    public double getAmount() {
+        return totalAmount;
+    }
+}
