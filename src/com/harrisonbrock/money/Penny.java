055bb9efd5317c80aba52dce8f94cac68099e02a
Harrison Brock <harrison@harrisonbrock.com>
2019-02-08 12:21:47 -0500

    Added Penny

---
+++ src/com/harrisonbrock/money/Penny.java
@@ -0,0 +1,32 @@
+package com.harrisonbrock.money;
+
+public class Penny implements Coin {
+    private double totalAmount;
+    private int amount;
+    private final double VALUE = 0.01;
+
+    public Penny() {
+        amount = 1;
+        this.totalAmount = amount * VALUE;
+    }
+
+    public Penny(int amount) {
+        this.amount = amount;
+        this.totalAmount= amount * VALUE;
+    }
+
+    @Override
+    public int amountInserted() {
+        return amount;
+    }
+
+    @Override
+    public String getName() {
+        return "Penny";
+    }
+
+    @Override
+    public double getAmount() {
+        return totalAmount;
+    }
+}
