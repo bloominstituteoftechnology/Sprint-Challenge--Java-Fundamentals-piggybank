6cf8b8b0746f1c2787a90f3c8fa81823a6739ef5
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 12:49:13 -0700

    Fixed some naming convention stuff, going to take a short break
    Ive types the word value so much it doesnt look or sound like a word anymore. i need to clear my head.

--- piggybank/src/piggybank/NationalBankOfPig.java
+++ piggybank/src/piggybank/NationalBankOfPig.java
@@ -1,11 +1,23 @@
 package piggybank;
 
+import java.util.ArrayList;
+
 public class NationalBankOfPig{
 
-	public NationalBankOfPig(ArrayList<MoneyActions> data){
-		for (MoneyActions money : array) {
+	private double value;
+	private ArrayList<AbstractMoney> data;
+
+	public double findTheValue(ArrayList<ChangeActions> toarray){
+		for (ChangeActions money : toArray) {
 			value += money.getValue();
+			// https://www.tutorialspoint.com/java/java_assignment_operators_examples.htm
 		} 
 		return value;
-	};
+	}
+
+	public NationalBankOfPig(ArrayList<ChangeActions> data){
+		this.data = data;
+		this.value = findTheValue(data);
+	}
+
 }
