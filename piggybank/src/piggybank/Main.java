054119ee525cf43cf46441e781412200d2334b23
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 10:52:49 -0700

    Update Main.java

--- piggybank/src/piggybank/Main.java
+++ piggybank/src/piggybank/Main.java
@@ -8,11 +8,18 @@ import java.text.DecimalFormat;
 public class Main 
 {
 	public static void main(String[] args)
-	{
 	// main method
+	{
+		DecimalFormat fp = new DecimalFormat("$###,###.00");
 
-		System.out.println("Testing");
-
-		Dollar 
+		// Quarter quarter = new Quarter(1);
+        // Dime dime = new Dime(1);
+        // Dollar dollar = new Dollar(5);
+        // Nickel nickel = new Nickel(3);
+        // Dime dime2 = new Dime(7);
+        // Dollar dollar2 = new Dollar(1);
+		// Penny penny = new Penny(10);
+		
+		System.out.println("*** National Bank of Pig ***");
 	}
 }
