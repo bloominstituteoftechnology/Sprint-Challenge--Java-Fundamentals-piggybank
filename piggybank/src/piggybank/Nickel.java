99b22bc590d1d1c4feadd28d79c90ee4fda52a98
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 12:33:13 -0700

    Finished nickel

--- piggybank/src/piggybank/Nickel.java
+++ piggybank/src/piggybank/Nickel.java
@@ -1,5 +1,31 @@
 package piggybank;
 
-public class Nickel {
+public class Nickel extends ChangeActions{
 	
+	public int ammount;
+	public double value;
+
+	public Nickel(int ammount){
+		this.ammount = ammount;
+		this.value = ammount * .25;
+	}
+	
+	@Override
+	public int getAmmount() {
+		return ammount;
+	}
+	
+	@Override
+	public double getValue(){
+		return value;
+	}
+
+	public void setAmmount(int ammount){
+		this.ammount = ammount;
+	}
+
+	@Override
+	public String toString(){
+		return ammount + "Nickel/s" + "\n";
+	}
 }
