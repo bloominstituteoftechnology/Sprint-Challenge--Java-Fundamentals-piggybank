f4c05964afccf3b0f1fcebe047bce0feb522922a
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 10:51:04 -0700

    Update Main.java

--- piggybank/src/piggybank/Main.java
+++ piggybank/src/piggybank/Main.java
@@ -1,8 +1,18 @@
 package piggybank;
 
-public class Main {
+import java.util.*;
+// WARNING: this imports the WHOLE package. importing the whole package can lead to a larger file size than needed.
+import java.text.DecimalFormat;
+// info > https://www2.cs.arizona.edu/classes/cs210/fall17/lectures/decimal_format.pdf
 
-	public static void main(String[] args){
+public class Main 
+{
+	public static void main(String[] args)
+	{
+	// main method
+
 		System.out.println("Testing");
+
+		Dollar 
 	}
 }
