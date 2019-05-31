34251cd37cf3027b9f56af488a6825f2b6bf5b38
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 12:30:50 -0700

    finished penny

--- piggybank/src/piggybank/Penny.java
+++ piggybank/src/piggybank/Penny.java
@@ -1,5 +1,31 @@
 package piggybank;
 
-public class Penny {
+public class Penny extends ChangeActions{
+
+	private double value;
+	private int ammount;
+
+	public Penny(int ammount){
+		this.ammount = ammount;
+		this.value = ammount * .01;
+	}
+
+	@Override
+	public int getAmmount() {
+		return ammount;
+	}
 	
+	@Override
+	public double getValue(){
+		return value;
+	}
+
+	public void setAmmount(int ammount){
+		this.ammount = ammount;
+	}
+
+	@Override
+	public String toString(){
+		return ammount + "Penny / Pennies" + "\n";
+	}
 }
