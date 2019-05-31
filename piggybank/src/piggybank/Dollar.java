f6226b14a615cc9639989d64d192be049bd19a9e
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 11:06:08 -0700

    Working on dollar

--- piggybank/src/piggybank/Dollar.java
+++ piggybank/src/piggybank/Dollar.java
@@ -4,5 +4,15 @@ public class Dollar extends MoneyActions{
 
 	private double value;
 	private int ammount;
-	
+	// private values, cant be accessed or seen by other classes, ONLY the parent and or child
+
+
+	public Dollar(int ammount){
+		this.ammount = ammount;
+	}
+
+	@Override
+	public double getAmmount(){
+		return ammount;
+	}
 }
