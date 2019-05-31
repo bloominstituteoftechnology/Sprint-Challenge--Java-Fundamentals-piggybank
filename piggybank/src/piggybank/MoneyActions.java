6f613b433f141d9953b6b38c695092c563bff9e6
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 11:12:53 -0700

    Update MoneyActions.java

--- piggybank/src/piggybank/MoneyActions.java
+++ piggybank/src/piggybank/MoneyActions.java
@@ -1,5 +1,15 @@
 package piggybank;
 
-public class MoneyActions {
+public abstract class MoneyActions {
+
+	private static int maxId = 0;
+	public int id;
 	
+	public MoneyActions(){
+		this.id = maxId++;
+		// ++ increments by 1
+	}
+
+	public abstract int getAmmount();
+	public
 }
