4eef7d7025639e011a6f27c5872d4d49443bef96
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 11:56:49 -0700

    Better internal file structure for error checking. Added onto ChangeActions

--- piggybank/src/piggybank/ChangeActions.java
+++ piggybank/src/piggybank/ChangeActions.java
@@ -1,5 +1,17 @@
 package piggybank;
 
-public abstract class ChangeActions{
-	
+public abstract class ChangeActions extends MoneyActions{
+
+	private String type;
+	public abstract double getAmmount();
+	public abstract int getValue();
+
+	public ChangeActions(){
+		this.type = "coin.";
+	};
+
+
+	public String getType(){
+		return type;
+	}
 }
--- piggybank/src/piggybank/MoneyActions.java
+++ piggybank/src/piggybank/MoneyActions.java
@@ -4,14 +4,14 @@ public abstract class MoneyActions {
 
 	private static int maxId = 0;
 	public int id;
+	public abstract double getAmmount();
+	public abstract int getValue();
 	
 	public MoneyActions(){
 		this.id = maxId++;
 		// ++ increments by 1
 	}
 
-	public abstract double getAmmount();
-	public abstract int getValue();
 	public int getId(){
 		return id;
 	}
