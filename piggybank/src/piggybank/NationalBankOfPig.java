96544c782039bc642dbee7d65ec416a0701d9d9f
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-31 12:39:58 -0700

    Working on data processing into an array.

---
+++ piggybank/src/piggybank/NationalBankOfPig.java
@@ -0,0 +1,11 @@
+package piggybank;
+
+public class NationalBankOfPig{
+
+	public NationalBankOfPig(ArrayList<MoneyActions> data){
+		for (MoneyActions money : array) {
+			value += money.getValue();
+		} 
+		return value;
+	};
+}
