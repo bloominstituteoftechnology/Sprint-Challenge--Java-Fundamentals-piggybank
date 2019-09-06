fd92bdb3ec0fd638954edcb638c8931afc5da707
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-09-06 11:20:44 -0500

    still setting up

---
+++ src/piggybank/Coin.java
@@ -0,0 +1,29 @@
+package piggybank;
+
+import java.util.*;
+
+
+public class Coin
+{
+	//attributes
+
+	private float coinValue; 
+	private int amountAdded;
+	// private float totalValue; 
+
+	//constructor 
+	public Coin(float coinValue, int amountAdded, /*float totalValue*/)
+	{
+		this.coinValue = coinValue;
+		this.amountAdded = amountAdded;
+		// this.totalValue = totalValue 
+	}
+
+	//methods
+
+	public void setTotalValue(float coinValue, float amountAdded)
+	{
+		
+	}
+
+}
---
+++ src/piggybank/Main.java
@@ -0,0 +1,16 @@
+package piggybank;
+
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class Main
+{
+	public static void main(String args [])
+	{
+		DecimalFormat fp = new DecimalFormat("$###,###.00");
+
+		//HashMap 
+		HashMap<Money, Amount> Bank = new HashMap <>();
+
+	}
+}
