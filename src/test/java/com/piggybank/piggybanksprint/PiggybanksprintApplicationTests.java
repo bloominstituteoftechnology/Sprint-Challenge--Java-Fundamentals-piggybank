6a8a8c564bf07649e0d256be799f951dbb691056
Tokiyn <tokiyn@yahoo.com>
2020-07-11 13:51:12 -0500

    MVP

---
+++ src/test/java/com/piggybank/piggybanksprint/PiggybanksprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggybank.piggybanksprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggybanksprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
