2c156f8742d9d169f43d820e9132000099649479
kodebae <karmenuopx@outlook.com>
2020-08-07 13:46:18 -0400

    set up complete now working on code

---
+++ src/main/java/com/java/piggybank/PiggybankApplication.java
@@ -0,0 +1,13 @@
+package com.java.piggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiggybankApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/java/piggybank/models/PiggyBank.java
@@ -0,0 +1,6 @@
+package com.java.piggybank.models;
+
+public class PiggyBank {
+    private long pbid;
+    private String
+}
