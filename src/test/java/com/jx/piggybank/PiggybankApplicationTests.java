887df9283e054e91bb952bbee79b439f6d114844
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-09-19 11:33:51 -0500

    feat: completed MVP

---
+++ src/test/java/com/jx/piggybank/PiggybankApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jx.piggybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggybankApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
