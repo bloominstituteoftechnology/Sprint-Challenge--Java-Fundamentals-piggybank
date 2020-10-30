6b8fc0668f8222bb5a754779f12d97254d8a9886
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-30 10:24:41 -0600

    Pig Model created and errors resolved

--- src/main/java/com/lambdaschool/piggybank/models/Pig.java
+++ src/main/java/com/lambdaschool/piggybank/models/Pig.java
@@ -11,6 +11,9 @@ package com.lambdaschool.piggybank.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "coins")
